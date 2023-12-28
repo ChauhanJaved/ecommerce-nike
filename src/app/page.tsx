a25921e9cb6deb348e14cfc55681d3d9e7613adb
Javed Chauhan <chauhanjaved@gmail.com>
2023-12-28 12:35:46 +0530

    Update: nike logo added to nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="text-center bg-red-500 text-white-400">Hello World</div>
+    <></>  
   )
 }
---
+++ src/app/ui/Nav.tsx
@@ -0,0 +1,14 @@
+
+const  Nav = () => {
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+        <nav>
+            <a href="/">
+                <img src="/images/header-logo.svg" alt="logo" width={130} height={29} />
+            </a>    
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
