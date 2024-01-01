b585bfbde3bcaf259a8a908284073fea8d75b66a
Javed Chauhan <chauhanjaved@gmail.com>
2024-01-01 12:18:11 +0530

    Update: Nav component updates

--- src/app/ui/Nav.tsx
+++ src/app/ui/Nav.tsx
@@ -1,14 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import { headerLogo } from "../../../public/images";
+import { navLinks } from '../utils/constants';
 
-const  Nav = () => {
+const Nav = () => {
   return (
-    <header className='padding-x py-8 absolute z-10 w-full'>
-        <nav>
-            <a href="/">
-                <img src="/images/header-logo.svg" alt="logo" width={130} height={29} />
-            </a>    
-        </nav>
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <Link href="/">
+          <Image src={headerLogo} alt="Logo" width={129} height={29} className='m-0 w-[129px] h-[29px]' />
+        </Link>
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <Link href={item.href}>
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
