b8c85b8b57e83ae5a3313923509f229089b0dcd0
Javed Chauhan <chauhanjaved@gmail.com>
2024-01-02 12:08:34 +0530

    Update: working on hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { Hero } from './sections'
 
 export default function Home() {
   return (
-    <></>  
+    <>
+      <Hero />
+    </>
   )
 }
